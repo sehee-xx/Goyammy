3a4178c8b437dfa4f750907e7b5e89c4a0a71744
sehee-xx <didtpgml0627@gmail.com>
2022-10-02 01:35:06 +0900

    Layout navigation UI 수정

--- src/components/commons/layout/navigation/LayoutNavigation.styles.ts
+++ src/components/commons/layout/navigation/LayoutNavigation.styles.ts
@@ -1,19 +1,18 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  height: 64px;
+  height: 50px;
   background-color: #f39c1f;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  font-size: 18px;
   color: white;
 `;
 
 export const MenuItem = styled.div`
   margin: 0px 60px;
-  font-size: 22px;
+  font-size: 18px;
   cursor: pointer;
   :hover {
     color: #464747;
