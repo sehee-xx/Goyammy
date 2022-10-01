637dcebcebf3d0d6e89d0d41cfafef259b9920e8
sehee-xx <didtpgml0627@gmail.com>
2022-10-02 01:01:30 +0900

    Layout Banner 하단 여백 제거

--- src/components/commons/layout/banner/LayoutBanner.styles.ts
+++ src/components/commons/layout/banner/LayoutBanner.styles.ts
@@ -1,11 +1,10 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  height: 400px;
+  height: 378px;
   background-color: white;
 `;
 
 export const SliderItem = styled.img`
-  height: 350px;
   margin: auto;
 `;
