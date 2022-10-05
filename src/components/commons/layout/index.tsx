51d40d7522a7f458e904a0bd8d644da693112794
sehee-xx <didtpgml0627@gmail.com>
2022-10-06 01:57:18 +0900

    Login, Signup 페이지에서 Banner 숨기기

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -3,6 +3,9 @@ import LayoutHeader from "./header/LayoutHeader.container";
 import LayoutNavigation from "./navigation/LayoutNavigation.container";
 import styled from "@emotion/styled";
 import { ReactNode } from "react";
+import { useRouter } from "next/router";
+
+const HIDDEN_HEADERS = ["/login", "/signup"];
 
 const Body = styled.div`
   width: 100%;
@@ -17,11 +20,14 @@ const Body = styled.div`
 interface ILayoutProps {
   children: ReactNode;
 }
+
 export default function Layout(props: ILayoutProps) {
+  const router = useRouter();
+  const isHiddenHeader = HIDDEN_HEADERS.includes(router.asPath);
   return (
     <>
       <LayoutHeader />
-      <LayoutBanner />
+      {!isHiddenHeader && <LayoutBanner />}
       <LayoutNavigation />
       <Body>{props.children}</Body>
     </>
