36abafaa3c810c20a5e057a2df559d831e29f6a1
sehee-xx <didtpgml0627@gmail.com>
2022-10-08 19:32:21 +0900

    Board write 페이지 border-radius, shadow 수정

--- src/components/units/board/write/BoardWrite.styles.ts
+++ src/components/units/board/write/BoardWrite.styles.ts
@@ -2,7 +2,6 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   width: 100%;
-  border: 1px solid black;
   padding-top: 80px;
   padding-bottom: 100px;
   padding-left: 102px;
@@ -10,8 +9,8 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border-radius: 30px;
-  border: 3px solid #cd863f;
+  border-radius: 10px;
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.07);
   background-color: white;
 `;
 
