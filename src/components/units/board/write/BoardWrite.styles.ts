722265bb153ba6da5606cb99b3279c031dabe854
sehee-xx <didtpgml0627@gmail.com>
2022-10-13 01:31:06 +0900

    Board Write 페이지 UI 수정

--- src/components/units/board/write/BoardWrite.styles.ts
+++ src/components/units/board/write/BoardWrite.styles.ts
@@ -1,11 +1,11 @@
 import styled from "@emotion/styled";
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
+export const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.form`
   width: 100%;
-  padding-top: 80px;
-  padding-bottom: 100px;
-  padding-left: 102px;
-  padding-right: 102px;
+  padding: 80px 120px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -15,85 +15,51 @@ export const Wrapper = styled.div`
 `;
 
 export const Title = styled.div`
-  font-family: Arial, Helvetica, sans-serif;
   font-size: 40px;
   font-weight: bold;
-  color: #8b4513;
+  color: #464747;
 `;
 
-export const WriterWrapper = styled.div`
+export const RowBox = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
   padding-top: 40px;
-  color: #8b4513;
-`;
-
-export const Writer = styled.input`
-  width: 486px;
-  height: 52px;
-  padding-left: 16px;
-  border-radius: 30px;
-  border: none;
-  background-color: #faf0e6;
-  color: #8b4513;
-  outline-color: #cd863f;
-  ::placeholder {
-    color: #8b4513;
-  }
-`;
-
-export const Password = styled.input`
-  width: 486px;
-  height: 52px;
-  padding-left: 16px;
-  border-radius: 30px;
-  border: none;
-  background-color: #faf0e6;
-  color: #8b4513;
-  outline-color: #cd863f;
-  ::placeholder {
-    color: #8b4513;
-  }
-`;
-
-export const Label = styled.div`
-  padding-bottom: 16px;
-  font-size: 20px;
-  font-weight: 500;
-  color: #8b4513;
+  gap: 30px;
 `;
 
 export const InputWrapper = styled.div`
-  padding-top: 40px;
-  color: #8b4513;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-top: 30px;
 `;
 
-export const Subject = styled.input`
-  width: 996px;
-  height: 52px;
-  padding-left: 16px;
-  border-radius: 30px;
-  border: none;
-  background-color: #faf0e6;
-  outline-color: #cd863f;
-  ::placeholder {
-    color: #8b4513;
-  }
+export const Label = styled.div`
+  padding-bottom: 5px;
+  font-size: 18px;
+  font-weight: 500;
+  color: #464747;
 `;
-export const Contents = styled.textarea`
-  width: 996px;
-  height: 480px;
+
+export const Contents = styled(ReactQuill)`
+  width: 100%;
+  height: 300px;
   padding-left: 16px;
   padding: 14px;
-  border-radius: 20px;
+  border-radius: 10px;
   resize: none;
-  border: none;
-  background-color: #faf0e6;
-  outline-color: #cd863f;
+  background-color: #f7f8f9;
+  color: #464747;
+  outline-color: #f39c1f;
   ::placeholder {
-    color: #8b4513;
+    color: #a5a5a5;
+  }
+  .ql-toolbar.ql-snow {
+    border: none !important;
+  }
+  .ql-container.ql-snow {
+    border: none !important;
   }
 `;
 
@@ -104,124 +70,60 @@ export const ZipcodeWrapper = styled.div`
 
 export const Zipcode = styled.input`
   width: 77px;
-  height: 52px;
+  height: 50px;
   padding-left: 16px;
-  border-radius: 30px;
+  border-radius: 10px;
   border: none;
-  background-color: #faf0e6;
-  color: red;
-  color: #8b4513;
-  outline-color: #cd863f;
+  background-color: #f7f8f9;
+  color: #464747;
+  outline: none;
 `;
 
 export const SearchButton = styled.button`
   width: 124px;
-  height: 52px;
+  height: 50px;
   margin-left: 16px;
-  background-color: orange;
-  border: 1px solid orange;
-  border-radius: 20px;
+  border: none;
+  background-color: #f39c1f;
+  border-radius: 10px;
   cursor: pointer;
   color: white;
 `;
 
 export const Address = styled.input`
-  width: 996px;
-  height: 52px;
-  margin-top: 16px;
+  width: 100%;
+  height: 50px;
   padding-left: 16px;
-  border-radius: 30px;
+  margin-bottom: 15px;
+  border-radius: 10px;
+  background-color: #f7f8f9;
   border: none;
-  background-color: #faf0e6;
-  color: #8b4513;
-  outline-color: #cd863f;
-`;
-
-export const Youtube = styled.input`
-  width: 996px;
-  height: 52px;
-  padding-left: 16px;
-  border-radius: 30px;
-  border: none;
-  background-color: #faf0e6;
-  color: #8b4513;
-  outline-color: #cd863f;
+  color: #464747;
+  outline-color: #f39c1f;
   ::placeholder {
-    color: #8b4513;
+    color: #a5a5a5;
   }
+  margin-top: 15px;
 `;
 
 export const ImageWrapper = styled.div`
-  width: 996px;
-  padding-top: 40px;
-  color: #8b4513;
   display: flex;
   flex-direction: row;
-`;
-
-export const OptionWrapper = styled.div`
-  width: 996px;
-  padding-top: 40px;
-  color: #8b4513;
-`;
-
-export const RadioButton = styled.input`
-  cursor: pointer;
-  color: #8b4513;
-  outline-color: #cd863f;
-  :checked {
-    accent-color: orange;
-  }
-`;
-
-export const RadioLabel = styled.label`
-  margin-left: 8px;
-  margin-right: 20px;
-  font-size: 20px;
-  font-weight: 500;
-  cursor: pointer;
-`;
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding-top: 80px;
-`;
-
-export const CancelButton = styled.button`
-  width: 179px;
-  height: 52px;
-  border-radius: 20px;
-  background-color: #ffdab9;
-  border-radius: 20px;
-  font-size: 16px;
-  font-weight: 500;
-  margin-left: 12px;
-  margin-right: 12px;
-  cursor: pointer;
 `;
 
 export const SubmitButton = styled.button`
-  width: 179px;
-  height: 52px;
+  width: 180px;
+  height: 60px;
   border: none;
-  border-radius: 20px;
-  font-size: 16px;
+  border-radius: 10px;
+  font-size: 18px;
   font-weight: 500;
-  margin-left: 12px;
-  margin-right: 12px;
+  margin-top: 100px;
   cursor: pointer;
-  background-color: orange;
+  background-color: #f39c1f;
   :disabled {
-    background-color: #ffdab9;
-    color: #8b4513;
+    background-color: #a5a5a5;
+    color: #fff;
   }
   color: white;
-`;
-
-export const Error = styled.div`
-  padding-top: 10px;
-  font-size: 14px;
-  color: red;
 `;
