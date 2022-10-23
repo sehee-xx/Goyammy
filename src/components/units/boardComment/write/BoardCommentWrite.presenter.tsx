8e031bed49c4f7a54eeb42286bbc8cb7f06773c9
sehee-xx <didtpgml0627@gmail.com>
2022-10-24 02:03:34 +0900

    BoardComment Write UI 수정

--- src/components/units/boardComment/write/BoardCommentWrite.presenter.tsx
+++ src/components/units/boardComment/write/BoardCommentWrite.presenter.tsx
@@ -5,10 +5,7 @@ export default function BoardCommentUI(props: IBoardCommentWriteUIProps) {
   return (
     <S.Wrapper>
       <S.Header>
-        <S.Top>
-          <S.TopImg></S.TopImg>
-          <S.TopText>댓글</S.TopText>
-        </S.Top>
+        <S.TopText>댓글</S.TopText>
         <S.WriterInfo>
           <S.Writer
             type="text"
@@ -24,20 +21,18 @@ export default function BoardCommentUI(props: IBoardCommentWriteUIProps) {
           ></S.Writer>
           <S.Star onChange={props.setStar} value={props.star} />
         </S.WriterInfo>
-        <S.Comment>
-          <S.CommentInput
-            maxLength={100}
-            onChange={props.onChangeContents}
-            placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
-            value={props.contents}
-          ></S.CommentInput>
-          <S.CommentBottom>
-            <S.CommentLimit>{props.contents.length}/100 </S.CommentLimit>
-            <S.CommentButton onClick={props.onClickWrite}>
-              등록하기
-            </S.CommentButton>
-          </S.CommentBottom>
-        </S.Comment>
+        <S.CommentInput
+          maxLength={100}
+          onChange={props.onChangeContents}
+          placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+          value={props.contents}
+        ></S.CommentInput>
+        <S.CommentBottom>
+          <S.CommentLimit>{props.contents.length}/100 </S.CommentLimit>
+          <S.CommentButton onClick={props.onClickWrite}>
+            등록하기
+          </S.CommentButton>
+        </S.CommentBottom>
       </S.Header>
     </S.Wrapper>
   );
