4b67f7cb7f5b23eec444ac743e74f5a3b5788d9b
sehee-xx <didtpgml0627@gmail.com>
2022-10-25 23:30:54 +0900

    Body 반응형 추가

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -4,6 +4,7 @@ import LayoutNavigation from "./navigation/LayoutNavigation.container";
 import styled from "@emotion/styled";
 import { ReactNode } from "react";
 import { useRouter } from "next/router";
+import { breakPoints } from "../breakPoints/breakPoints";
 
 const HIDDEN_HEADERS = ["/login", "/signup"];
 
@@ -15,6 +16,14 @@ const Body = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 30px 0px 150px 0px;
+
+  @media ${breakPoints.tablet} {
+    padding: 20px 20px 150px 20px;
+  }
+
+  @media ${breakPoints.mobile} {
+    padding: 20px 20px 150px 20px;
+  }
 `;
 
 interface ILayoutProps {
