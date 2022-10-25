b96fd822855a5cbaabcbc52c0dadabce8a23bb03
sehee-xx <didtpgml0627@gmail.com>
2022-10-25 23:42:43 +0900

    CATS 페이지 반응형 추가

--- src/components/units/cats/list/CatsList.styles.ts
+++ src/components/units/cats/list/CatsList.styles.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { breakPoints } from "../../../commons/breakPoints/breakPoints";
 
 export const Wrapper = styled.div`
   width: 100%;
@@ -10,4 +11,12 @@ export const Wrapper = styled.div`
 export const CatImg = styled.img`
   width: 33.33%;
   height: 420px;
+
+  @media ${breakPoints.tablet} {
+    height: 250px;
+  }
+
+  @media ${breakPoints.mobile} {
+    height: 150px;
+  }
 `;
