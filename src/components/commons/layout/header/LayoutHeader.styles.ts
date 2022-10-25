bc31fe9708aec3f8b5f3c6d9ed483aaa42363402
sehee-xx <didtpgml0627@gmail.com>
2022-10-25 23:00:08 +0900

    Layout Header 반응형 추가

--- src/components/commons/layout/header/LayoutHeader.styles.ts
+++ src/components/commons/layout/header/LayoutHeader.styles.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { breakPoints } from "../../breakPoints/breakPoints";
 
 export const Wrapper = styled.div`
   height: 70px;
@@ -6,6 +7,15 @@ export const Wrapper = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
+
+  @media ${breakPoints.tablet} {
+    height: 50px;
+    padding: 0px 30px;
+  }
+
+  @media ${breakPoints.mobile} {
+    height: 50px;
+  }
 `;
 
 export const InnerWrapper = styled.div`
@@ -21,6 +31,14 @@ export const InnerWrapper = styled.div`
 export const InnerLogo = styled.img`
   height: 50px;
   cursor: pointer;
+
+  @media ${breakPoints.tablet} {
+    height: 30px;
+  }
+
+  @media ${breakPoints.mobile} {
+    height: 30px;
+  }
 `;
 
 export const RowBox = styled.div`
@@ -28,6 +46,14 @@ export const RowBox = styled.div`
   flex-direction: row;
   align-items: center;
   gap: 20px;
+
+  @media ${breakPoints.tablet} {
+    gap: 10px;
+  }
+
+  @media ${breakPoints.mobile} {
+    gap: 8px;
+  }
 `;
 
 export const Profile = styled.div`
@@ -39,6 +65,14 @@ export const Profile = styled.div`
 
 export const ProfileImg = styled.img`
   width: 30px;
+
+  @media ${breakPoints.tablet} {
+    width: 20px;
+  }
+
+  @media ${breakPoints.mobile} {
+    width: 20px;
+  }
 `;
 
 export const UserName = styled.div`
@@ -49,6 +83,16 @@ export const UserName = styled.div`
     color: #f39c1f;
   }
   cursor: pointer;
+
+  @media ${breakPoints.tablet} {
+    font-size: 12px;
+    font-weight: 600;
+  }
+
+  @media ${breakPoints.mobile} {
+    font-size: 10px;
+    font-weight: 600;
+  }
 `;
 
 export const Authentication = styled.div`
@@ -59,4 +103,14 @@ export const Authentication = styled.div`
     color: #f39c1f;
   }
   cursor: pointer;
+
+  @media ${breakPoints.tablet} {
+    font-size: 12px;
+    font-weight: 600;
+  }
+
+  @media ${breakPoints.mobile} {
+    font-size: 10px;
+    font-weight: 600;
+  }
 `;
