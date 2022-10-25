707ec56655bc9c1b2d798d7a83a05bd51d80669f
sehee-xx <didtpgml0627@gmail.com>
2022-10-25 22:31:07 +0900

    kakaoMap script type 추가

--- src/components/commons/kakao-map/write/index.tsx
+++ src/components/commons/kakao-map/write/index.tsx
@@ -7,6 +7,7 @@ declare const window: typeof globalThis & {
 export default function KakaoMapWrite(props: any) {
   useEffect(() => {
     const script = document.createElement("script");
+    script.type = "text/javascript";
     script.src =
       "//dapi.kakao.com/v2/maps/sdk.js?appkey=bf1abfa34e8657abbd85bce3a25b1c4d&autoload=false&libraries=services";
     document.head.appendChild(script);
