16fee3f68eec4f0c1860a4ff666cc0feeb2cd984
sehee-xx <didtpgml0627@gmail.com>
2022-10-25 22:30:30 +0900

    Layout Navigation 반응형

--- src/components/commons/layout/navigation/LayoutNavigation.styles.ts
+++ src/components/commons/layout/navigation/LayoutNavigation.styles.ts
@@ -1,12 +1,22 @@
 import styled from "@emotion/styled";
+import { breakPoints } from "../../breakPoints/breakPoints";
 
 export const Wrapper = styled.div`
+  width: 100%;
   height: 50px;
   background-color: #f39c1f;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+
+  @media ${breakPoints.tablet} {
+    height: 40px;
+  }
+
+  @media ${breakPoints.mobile} {
+    height: 30px;
+  }
 `;
 
 export const MenuItem = styled.div`
@@ -17,5 +27,15 @@ export const MenuItem = styled.div`
     props.isValid ? "#464747" : "#fff"};
   :hover {
     color: #464747;
+  }
+
+  @media ${breakPoints.tablet} {
+    margin: 0px 40px;
+    font-size: 14px;
+  }
+
+  @media ${breakPoints.mobile} {
+    margin: 0px 14px;
+    font-size: 8px;
   }
 `;
