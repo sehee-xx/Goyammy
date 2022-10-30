36a68979b5a8ebf14a297c33de136bc9908ea8c8
sehee-xx <didtpgml0627@gmail.com>
2022-10-30 23:54:56 +0900

    Market Comment Question Edit UI 수정

--- src/components/units/useditem-question/edit/UseditemQuestionEdit.presenter.tsx
+++ src/components/units/useditem-question/edit/UseditemQuestionEdit.presenter.tsx
@@ -6,36 +6,34 @@ export default function UseditemQuestionUI(
 ) {
   return (
     <S.Wrapper>
-      <S.Page>
-        <S.Header>
-          <S.Top>
-            <S.TopImg></S.TopImg>
-            <S.TopText>문의 수정</S.TopText>
-          </S.Top>
-          <S.WriterInfo>
-            <S.Writer
-              type="text"
-              placeholder="작성자"
-              defaultValue={props.el.user.name}
-              readOnly
-            ></S.Writer>
-          </S.WriterInfo>
-          <S.Comment>
-            <S.CommentInput
-              maxLength={100}
-              onChange={props.onChangeContents}
-              placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
-              defaultValue={props.el.contents}
-            ></S.CommentInput>
-            <S.CommentBottom>
-              <S.CommentLimit>{props.editContents.length}/100 </S.CommentLimit>
-              <S.CommentButton onClick={props.onClickUpdate}>
-                수정하기
-              </S.CommentButton>
-            </S.CommentBottom>
-          </S.Comment>
-        </S.Header>
-      </S.Page>
+      <S.Header>
+        <S.Top>
+          <S.TopText>문의 수정</S.TopText>
+          <S.CancelIcon src="/images/grayX.png" onClick={props.onClickCancel} />
+        </S.Top>
+        <S.WriterInfo>
+          <S.Writer
+            type="text"
+            placeholder="작성자"
+            defaultValue={props.el.user.name}
+            readOnly
+          ></S.Writer>
+        </S.WriterInfo>
+        <S.Comment>
+          <S.CommentInput
+            maxLength={100}
+            onChange={props.onChangeContents}
+            placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+            defaultValue={props.el.contents}
+          ></S.CommentInput>
+          <S.CommentBottom>
+            <S.CommentLimit>{props.editContents.length}/100 </S.CommentLimit>
+            <S.CommentButton onClick={props.onClickUpdate}>
+              수정하기
+            </S.CommentButton>
+          </S.CommentBottom>
+        </S.Comment>
+      </S.Header>
     </S.Wrapper>
   );
 }
