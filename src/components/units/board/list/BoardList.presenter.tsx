bb2687ec1754f2138f4f4af14b3738699572abee
sehee-xx <didtpgml0627@gmail.com>
2022-11-09 23:57:17 +0900

    Board List Page UI 수정

--- src/components/units/board/list/BoardList.presenter.tsx
+++ src/components/units/board/list/BoardList.presenter.tsx
@@ -7,65 +7,57 @@ import Searchbars01 from "../../../commons/searchbars/01/Searchbars01.container"
 export default function BoardListUI(props: IBoardListUIProps) {
   return (
     <S.Wrapper>
-      <S.Main>
-        <S.HeaderText>베스트 게시글</S.HeaderText>
-        <S.Body>
-          <S.BestList>
-            <S.BestBox src="/images/cat1.jpeg/"></S.BestBox>
-            <S.BestBox src="/images/cat2.jpeg/"></S.BestBox>
-            <S.BestBox src="/images/cat3.jpeg/"></S.BestBox>
-            <S.BestBox src="/images/cat4.png/"></S.BestBox>
-          </S.BestList>
-          <Searchbars01
-            refetch={props.refetch}
-            refetchBoardsCount={props.refetchBoardsCount}
-            onChangeKeyword={props.onChangeKeyword}
-          />
-          <S.Table>
-            <S.TableTop />
-            <S.RowTop>
-              <S.ColumnHeaderBasic>ID</S.ColumnHeaderBasic>
-              <S.ColumnHeaderTitle>제목</S.ColumnHeaderTitle>
-              <S.ColumnHeaderBasic>작성자</S.ColumnHeaderBasic>
-              <S.ColumnHeaderBasic>날짜</S.ColumnHeaderBasic>
-            </S.RowTop>
-            {props.data?.fetchBoards.map((el: any) => (
-              <S.Row key={uuidv4()}>
-                <S.ColumnBasic>
-                  {String(el._id).slice(-4).toUpperCase()}
-                </S.ColumnBasic>
-                <S.ColumnTitle
-                  id={el._id}
-                  onClick={props.onClickMoveToBoardDetail}
-                >
-                  {el.title
-                    .replaceAll(props.keyword, `@#$%${props.keyword}@#$%`)
-                    .split("@#$%")
-                    .map((el: any) => (
-                      <S.TextToken
-                        key={uuidv4()}
-                        isMatched={props.keyword === el}
-                      >
-                        {el}
-                      </S.TextToken>
-                    ))}
-                </S.ColumnTitle>
-                <S.ColumnBasic>{el.writer}</S.ColumnBasic>
-                <S.ColumnBasic>
-                  {el.createdAt.slice(0, 10).replace(/-/gi, ".")}
-                </S.ColumnBasic>
-              </S.Row>
-            ))}
-            <S.TableBottom />
-          </S.Table>
-        </S.Body>
-        <S.Footer>
-          <Paginations01 refetch={props.refetch} count={props.count} />
-          <S.SignupButton onClick={props.onClickWriteBoard}>
-            등록하기
-          </S.SignupButton>
-        </S.Footer>
-      </S.Main>
+      <S.HeaderText>베스트 게시글</S.HeaderText>
+      <S.Body>
+        <S.BestList>
+          <S.BestImg src="/images/cat2.jpeg/"></S.BestImg>
+          <S.BestImg src="/images/cat1.jpeg/"></S.BestImg>
+          <S.BestImg src="/images/cat3.jpeg/"></S.BestImg>
+          <S.BestImg src="/images/cat4.png/"></S.BestImg>
+        </S.BestList>
+        <Searchbars01
+          refetch={props.refetch}
+          refetchBoardsCount={props.refetchBoardsCount}
+          onChangeKeyword={props.onChangeKeyword}
+        />
+        <S.Table>
+          <S.TableTop />
+          <S.RowTop>
+            <S.ColumnHeaderTitle>제목</S.ColumnHeaderTitle>
+            <S.ColumnHeaderWriter>작성자</S.ColumnHeaderWriter>
+            <S.ColumnHeaderDate>날짜</S.ColumnHeaderDate>
+          </S.RowTop>
+          {props.data?.fetchBoards.map((el: any) => (
+            <S.Row key={uuidv4()}>
+              <S.ColumnTitle
+                id={el._id}
+                onClick={props.onClickMoveToBoardDetail}
+              >
+                {el.title
+                  .replaceAll(props.keyword, `@#$%${props.keyword}@#$%`)
+                  .split("@#$%")
+                  .map((el: any) => (
+                    <S.TextToken
+                      key={uuidv4()}
+                      isMatched={props.keyword === el}
+                    >
+                      {el}
+                    </S.TextToken>
+                  ))}
+              </S.ColumnTitle>
+              <S.ColumnWriter>{el.writer}</S.ColumnWriter>
+              <S.ColumnDate>
+                {el.createdAt.slice(0, 10).replace(/-/gi, ".")}
+              </S.ColumnDate>
+            </S.Row>
+          ))}
+          <S.TableBottom />
+        </S.Table>
+      </S.Body>
+      <S.Footer>
+        <Paginations01 refetch={props.refetch} count={props.count} />
+        <S.SignupButton onClick={props.onClickWriteBoard}>등록</S.SignupButton>
+      </S.Footer>
     </S.Wrapper>
   );
 }
--- src/components/units/board/list/BoardList.styles.ts
+++ src/components/units/board/list/BoardList.styles.ts
@@ -4,10 +4,6 @@ import { ITextTokenProps } from "./BoardList.types";
 export const Wrapper = styled.div`
   width: 100%;
   max-width: 1247px;
-`;
-export const Main = styled.div`
-  width: 100%;
-  background-color: linen;
   border-radius: 10px;
   box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.15);
   display: flex;
@@ -15,15 +11,16 @@ export const Main = styled.div`
   justify-content: center;
   align-items: center;
   background-color: white;
-  padding: 40px 50px 80px 50px;
+  padding: 80px 120px;
 `;
+
 export const HeaderText = styled.div`
-  font-size: 36px;
+  font-size: 40px;
   font-weight: 700;
-  color: black;
   padding-bottom: 40px;
-  color: #8b4513;
+  color: #464747;
 `;
+
 export const Body = styled.div`
   width: 100%;
   display: flex;
@@ -31,81 +28,40 @@ export const Body = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
 export const BestList = styled.div`
+  width: 100%;
+  max-width: 1007px;
   display: flex;
   flex-direction: row;
-  padding-bottom: 80px;
+  justify-content: space-between;
+  padding-bottom: 60px;
 `;
-export const BestBox = styled.img`
-  width: 282px;
-  height: 257px;
-  background-color: gray;
-  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
-  border-radius: 20px;
-  margin: 0px 12px;
-`;
-export const Date = styled.input`
-  width: 244px;
-  height: 52px;
-  color: #8b4513;
-  outline-color: #cd863f;
-  background-color: #faf0e6;
-  ::placeholder {
-    color: #8b4513;
-  }
-  border-radius: 10px;
-  border: none;
-  padding-left: 20px;
-  margin-right: 44px;
-`;
-export const BoardLists = styled.div`
-  width: 100%;
-  height: 583px;
-  background-color: white;
-  margin-bottom: 40px;
-`;
-export const LabelBar = styled.div``;
-export const Label = styled.label``;
 
-export const Column = styled.div`
-  width: 20%;
+export const BestImg = styled.img`
+  max-width: 230px;
+  aspect-ratio: 1;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
 `;
-export const ListContents = styled.div``;
-export const Footer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-export const SignupButton = styled.button`
-  width: 171px;
-  height: 52px;
-  border: none;
+
+export const Table = styled.div`
+  width: 100%;
+  padding-bottom: 40px;
+  color: #464747;
   font-size: 16px;
-  font-weight: 700px;
-  background-color: #ffdab9;
-  border-radius: 30px;
-  margin-top: 80px;
-  color: #8b4513;
-  cursor: pointer;
-  :hover {
-    background-color: orange;
-    color: white;
-  }
 `;
 
 export const TableTop = styled.div`
   margin-top: 20px;
 `;
 
-export const TableBottom = styled.div``;
-
 export const RowTop = styled.div`
   display: flex;
   flex-direction: row;
   height: 52px;
   line-height: 52px;
-  border-bottom: 2px solid #cd863f;
+  border-bottom: 1.5px solid #a5a5a5;
 `;
 
 export const Row = styled(RowTop)`
@@ -115,38 +71,67 @@ export const Row = styled(RowTop)`
   }
 `;
 
-export const ColumnHeaderBasic = styled.div`
-  width: 10%;
-  text-align: center;
-`;
-
 export const ColumnHeaderTitle = styled.div`
-  width: 70%;
+  width: 25%;
   text-align: center;
 `;
 
-export const ColumnBasic = styled.div`
-  width: 10%;
+export const ColumnHeaderWriter = styled.div`
+  width: 50%;
+  text-align: center;
+`;
+
+export const ColumnHeaderDate = styled.div`
+  width: 25%;
+  text-align: center;
+`;
+
+export const ColumnWriter = styled.div`
+  width: 50%;
+  text-align: center;
+`;
+
+export const ColumnDate = styled.div`
+  width: 25%;
   text-align: center;
 `;
 
 export const ColumnTitle = styled.div`
-  width: 70%;
+  width: 25%;
   text-align: center;
   cursor: pointer;
   :hover {
-    color: orange;
+    color: #f39c1f;
   }
 `;
 
-export const Table = styled.div`
-  width: 1200px;
-  padding-bottom: 40px;
-  color: #8b4513;
-  font-size: 16px;
+export const TextToken = styled.span`
+  color: ${(props: ITextTokenProps) => (props.isMatched ? "#f39c1f" : "")};
+  font-weight: ${(props: ITextTokenProps) => (props.isMatched ? "bold" : "")};
 `;
 
-export const TextToken = styled.span`
-  color: ${(props: ITextTokenProps) => (props.isMatched ? "orange" : "")};
-  font-weight: ${(props: ITextTokenProps) => (props.isMatched ? "bold" : "")};
+export const TableBottom = styled.div``;
+
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SignupButton = styled.button`
+  width: 180px;
+  height: 60px;
+  border: none;
+  font-size: 18px;
+  font-weight: 500px;
+  background-color: #a5a5a5;
+  border-radius: 10px;
+  margin-top: 100px;
+  color: #fff;
+  cursor: pointer;
+  :hover {
+    background-color: #f39c1f;
+    color: white;
+  }
 `;
