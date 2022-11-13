5b6296d2d2cb02a7c011effe41d444e896429661
sehee-xx <didtpgml0627@gmail.com>
2022-11-14 03:16:33 +0900

    판매중, 판매완료 기능 타입 추가

--- src/components/units/useditem/list/ItemList.types.ts
+++ src/components/units/useditem/list/ItemList.types.ts
@@ -1,7 +1,9 @@
 export interface IItemsListUIProps {
   data: any;
   visitedItems: any;
+  isSoldout: string;
   onClickMoveToNew: () => void;
   onClickMoveToDetail: (el: any) => () => void;
   loadFunc: () => void;
+  onChangeIsSoldout: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
