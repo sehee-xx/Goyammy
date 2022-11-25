95498d725ca9f8bef86423e21b393b974f2d2382
sehee-xx <didtpgml0627@gmail.com>
2022-11-26 00:38:54 +0900

    onChangeSearchbar  화살표 함수로 변경

--- src/components/commons/searchbars/01/Searchbars01.container.tsx
+++ src/components/commons/searchbars/01/Searchbars01.container.tsx
@@ -1,18 +1,18 @@
-import _ from "lodash"
+import _ from "lodash";
 import { ChangeEvent } from "react";
 import Searchbars01UI from "./Searchbars01.presenter";
-import { ISearchbars01Props } from "./Searchbars01.types"
+import { ISearchbars01Props } from "./Searchbars01.types";
 
 export default function Searchbars01(props: ISearchbars01Props) {
   const getDebounce = _.debounce((data: string) => {
-    props.refetch({ search: data, page: 1});
+    props.refetch({ search: data, page: 1 });
     props.refetchBoardsCount({ search: data });
     props.onChangeKeyword(data);
-  }, 200)
+  }, 200);
 
-  function onChangeSearchbar(event: ChangeEvent<HTMLInputElement>) {
+  const onChangeSearchbar = (event: ChangeEvent<HTMLInputElement>) => {
     getDebounce(event.currentTarget.value);
-  }
+  };
 
-  return <Searchbars01UI onChangeSearchbar={onChangeSearchbar} />
+  return <Searchbars01UI onChangeSearchbar={onChangeSearchbar} />;
 }
