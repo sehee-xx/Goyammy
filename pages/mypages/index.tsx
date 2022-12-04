b34e45a8f6daac9a80c24e222670818aada9d336
sehee-xx <didtpgml0627@gmail.com>
2022-12-04 23:18:47 +0900

    Mypage 폴더 위치 변경

--- pages/mypages/index.tsx
+++ pages/mypages/index.tsx
@@ -1,6 +1,6 @@
 // 마이페이지
 import { useAuth } from "../../src/components/commons/hooks/useAuth";
-import ItemMyPage from "../../src/components/units/useditem/mypage/ItemMypage.container";
+import ItemMyPage from "../../src/components/units/mypage/ItemMypage.container";
 
 export default function MyPage() {
   useAuth();
