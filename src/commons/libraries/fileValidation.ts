6c97a7b31a80c6b28038b441c9b670925d434af8
sehee-xx <didtpgml0627@gmail.com>
2022-12-05 01:58:20 +0900

    주석 삭제

--- src/commons/libraries/fileValidation.ts
+++ src/commons/libraries/fileValidation.ts
@@ -4,13 +4,11 @@ export const checkFileValidation = (file?: File) => {
     return false;
   }
 
-  // 파일 크기가 5MB가 넘는 경우
   if (file.size > 5 * 1024 * 1024) {
     alert("파일 용량이 너무 큽니다. (제한: 5MB)");
     return false;
   }
 
-  // 파일 타입을 지정
   if (!file.type.includes("jpeg") && !file.type.includes("png")) {
     alert("jpeg 파일 또는 png 파일만 업로드 가능합니다.");
     return false;
