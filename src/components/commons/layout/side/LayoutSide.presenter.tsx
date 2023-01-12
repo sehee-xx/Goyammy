3d0943d9d344b4e0de6de0329a0b1909be3cc595
sehee-xx <didtpgml0627@gmail.com>
2023-01-12 23:25:03 +0900

    LayoutSide CheckImgUrl 함수 추가

--- src/components/commons/layout/side/LayoutSide.presenter.tsx
+++ src/components/commons/layout/side/LayoutSide.presenter.tsx
@@ -3,17 +3,23 @@ import { v4 as uuidv4 } from "uuid";
 import { ILayoutSideProps } from "./LayoutSide.types";
 
 export default function LayoutSideUI(props: ILayoutSideProps) {
+  const CheckImageUrl = (index: number) => {
+    if (
+      props.visited[index]?.images?.length > 0 &&
+      props.visited[index]?.images[0]
+    ) {
+      return `https://storage.googleapis.com/${props.visited[index]?.images[0]}`;
+    }
+    return "/images/noImage.jpeg";
+  };
+
   return (
     <S.Wrapper>
       <S.Today>최근 본 상품</S.Today>
       {new Array(3).fill(1).map((_, index) => (
         <div key={uuidv4()}>
           <S.TodayItem
-            src={
-              props.visited[index]?.images
-                ? `https://storage.googleapis.com/${props.visited[index]?.images[0]}`
-                : "/images/noImage.jpeg"
-            }
+            src={CheckImageUrl(index)}
             onClick={() => props.onClickMoveToVisited(props.visited[index])}
           />
         </div>
